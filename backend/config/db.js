f0146d2d9e8a977ee14403a240461fd0653e5a29
seeubot <155578467+seeubot@users.noreply.github.com>
2026-05-29 13:09:34 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`MongoDB Error: ${error.message}`);
     process.exit(1);
   }
 };
