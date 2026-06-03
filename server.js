835e25eb32a96b26856541c34fcb14e339c1aa95
seeubot <155578467+seeubot@users.noreply.github.com>
2026-06-03 14:27:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -451,6 +451,7 @@ app.post('/api/upload/catbox', authenticate, upload.single('file'), async (req, 
       headers: {
         ...form.getHeaders(),
         'Content-Length': contentLength,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
       },
       timeout:          60000,
       maxContentLength: Infinity,
